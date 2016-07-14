6466ae1b8a053163f0c63e46c139707bcd38177e
Tatiana Bernardo <tatiana.mobe@gmail.com>
2016-07-15 00:10:13 +0100

    Initial game transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,35 @@
 var gold = 0;
-var name = prompt("Please enter your daugther name", "Olive");
-
-document.getElementById("name").innerHTML = name;
+var age = 10;
+var name;
+var birthday;
+var bloodType;
 
 function jobClick(num){
   gold += num;
-  document.getElementById("gold").innerHTML = gold;
+  var findGold = document.getElementsByClassName("gold");
+  for (var i = 0; i < findGold.length; i++) {
+    findGold[i].innerHTML = gold;
+  }
+  if (gold === 500){
+    document.getElementById("beggining").className += "hidden";
+    document.getElementById("questions").className = document.getElementById("questions").className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+  }
+};
+
+function startGame(){
+
+   name = document.forms["initialSetup"]["name"].value;
+   birthday = document.forms["initialSetup"]["birthday"].value;
+   bloodType = document.forms["initialSetup"]["bloodType"].value;
+
+   document.getElementById("name").innerHTML = name;
+   document.getElementById("birthday").innerHTML = birthday;
+
+  document.getElementById("questions").className += "hidden";
+
+  document.getElementById("princess").className = document.getElementById("princess").className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+  document.getElementById("attributes").className = document.getElementById("attributes").className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+  document.getElementById("jobsandlessons").className = document.getElementById("jobsandlessons").className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+
+  event.preventDefault();
 };
